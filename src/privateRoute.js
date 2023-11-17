9681b59a3e88064cac444e3df5282ff1103d406c
noguful <noguchi.shin1@gmail.com>
2023-11-18 00:12:36 +0900

    Routes Route の elementを使うように変更。合わせてサインインのリダイレクト対応

---
+++ src/privateRoute.js
@@ -0,0 +1,8 @@
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from 'react-router-dom';
+
+export const PrivateRoute = () => {
+  const auth = useSelector((state) => state.auth.isSignIn)
+
+  return auth ? <Outlet /> : <Navigate to="/signin" />;
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { NotFound } from "../pages/NotFound";
 import { SignIn } from "../pages/SignIn";
@@ -9,28 +8,24 @@ import { NewList } from "../pages/NewList";
 import { EditTask } from "../pages/EditTask";
 import { SignUp } from "../pages/SignUp";
 import { EditList } from "../pages/EditList";
+import { PrivateRoute } from "../privateRoute";
 
 export const Router = () => {
-  const auth = useSelector((state) => state.auth.isSignIn)
 
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
-        {auth ? (
-          <>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/task/new" component={NewTask} />
-            <Route exact path="/list/new" component={NewList} />
-            <Route exact path="/lists/:listId/tasks/:taskId" component={EditTask} />
-            <Route exact path="/lists/:listId/edit" component={EditList} />
-          </>
-        ) : (
-          <Redirect to="/signin" />
-        )}
-        <Route component={NotFound} />
-      </Switch>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path='/' element={<PrivateRoute/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/task/new" element={<NewTask />} />
+          <Route path="/list/new" element={<NewList />} />
+          <Route path="/lists/:listId/tasks/:taskId" element={<EditTask />} />
+          <Route path="/lists/:listId/edit" element={<EditList />} />
+        </Route>
+        <Route element={<NotFound/> } />
+      </Routes>
     </BrowserRouter>
   )
 }
